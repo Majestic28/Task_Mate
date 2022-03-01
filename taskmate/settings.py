29489ce0fe883ebc4433a21ad37a5653baceffbe
gkl <gokulrajakalappan@gmail.com>
2022-03-01 11:38:17 +0530

    updated files

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -118,10 +118,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
-
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "todo"
 LOGIN_URL = "login"
 
 django_heroku.settings(locals())
+
